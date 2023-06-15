097869b6fda477360bfcaf8cd938aef849696fca
Kelebegirl <amaryambamidele@gmail.com>
2023-06-15 14:54:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that print line with function puts
+ * Return: 0 (Good)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
