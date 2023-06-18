e15dc7761e76c2154405a0b81998133361990df8
Kelebegirl <amaryambamidele@gmail.com>
2023-06-18 12:26:48 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Good)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
