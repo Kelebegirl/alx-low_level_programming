0d71d983f003d706a6bfd8dbd50906bcbdb70687
Kelebegirl <amaryambamidele@gmail.com>
2023-06-18 09:40:07 -0700

    putchar commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: A
+ * Return: Always 0 (Good)
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'g';
+		while
+			(d <= 'i') {
+				putchar(d);
+				d++;
+			}
+	putchar('\n');
+	return (0);
+}
