e5ac8b0d2154416f286f897e67e89947c9e8958d
Kelebegirl <amaryambamidele@gmail.com>
2023-06-18 10:23:46 -0700

    uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Good)
+ */
+
+int main(void)
+{
+	char f, l;
+
+	f = 'a';
+	l = 'A';
+	while
+		(f <= 'z') {
+			putchar(f);
+			f++;
+		}
+	while
+		(l <= 'Z') {
+			putchar(l);
+			l++;
+		}
+	putchar('\n');
+	return (0);
+}
