d5804813ce224bbe1fbe6fddde5a908b3e7eb08e
Kelebegirl <amaryambamidele@gmail.com>
2023-06-21 13:26:20 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - function that return thee absolute value of given integers
+ * @d: declaration of input function
+ * Return: 0 (Good)
+ */
+
+int _abs(int d)
+{
+	if (d < 0)
+		d = (-1) * d;
+	return (d);
+}
