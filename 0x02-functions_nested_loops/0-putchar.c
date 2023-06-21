9734453f3789f7d19998cd934a38fab35b2c3858
Kelebegirl <amaryambamidele@gmail.com>
2023-06-20 20:52:05 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 (Good)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('u');
+	_putchar('c');
+	_putchar('p');
+	_putchar('t');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
