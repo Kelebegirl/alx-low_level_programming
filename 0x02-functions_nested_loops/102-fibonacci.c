55cc87465fc522fa07164b898779d3ebdcfd5cb8
Kelebegirl <amaryambamidele@gmail.com>
2023-06-24 09:57:30 -0700

    correct

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int count;
-
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
 	for (count = 0; count < 50; count++)
@@ -23,7 +22,7 @@ int main(void)
 		if (count == 49)
 			printf('\n');
 		else
-			printf(' ');
+			printf(", ");
 	}
 	return (0);
 }
