7449a14af0d35f0b8114d94e99fb67f1874193c9
Kelebegirl <amaryambamidele@gmail.com>
2023-06-24 09:43:23 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,4 +10,29 @@
 
 void times_table(void)
 {
-	int 
+	int num, x, y;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for (x = 0; x <= 9; x++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			y = num * x;
+
+			/**
+			 * put space if y is a single number
+			 * place the first digit of its 2 number
+			 */
+			if (y <= 9)
+				_putchar(' ');
+			else
+				_putchar((y / 10) + 48); /*the first digit*/
+
+			_putchar((y % 10) + 48); /*the second  digit*/
+		}
+		_putchar('\n');
+	}
+}
