30e40d59139b7ff9f0b3f6a700e569748409fb27
Kelebegirl <amaryambamidele@gmail.com>
2023-06-27 13:41:44 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - function yhat prints a string in reverse
+ * @s: string
+ *
+ * Return: Always 0 (Good)
+ */
+
+void print_rev(char *s)
+{
+	int k = 0;
+
+	while (s[k])
+	{
+		k++;
+	}
+
+	while (k--)
+	{
+		_putchar(s[k]);
+	}
+	_putchar('\n');
+}
