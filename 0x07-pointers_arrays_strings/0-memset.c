c3c4de7a2b1b8caf856fbc7d13fc5febc18718ce
Kelebegirl <amaryambamidele@gmail.com>
2023-07-03 19:45:43 -0700

    ./0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy
+ *
+ * Return: s
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+
+	return (s);
+}
